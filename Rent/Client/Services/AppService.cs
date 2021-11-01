6bd6a29a0ce40655770d3d5a2254190536e99ee2
Alexander Solomin <solo_true@yahoo.com>
2021-11-01 21:04:27 +0500

    added search

---
+++ Rent/Client/Services/AppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Rent.Client.Services
+{
+    public class AppService
+    {
+        public AppService()
+        {
+        }
+    }
+}
---
+++ Rent/Client/Services/ICitiesService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Rent.Client.Services
+{
+    public interface ICitiesService
+    {
+    }
+}
